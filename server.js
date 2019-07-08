05c0faeb444b142192a374d927ec02b874bc360b
Sondre W. K-Knutsen <knutsensondre@gmail.com>
2019-07-09 00:02:15 +0200

    Add server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, res){
+    res.json({"hello" : "world"});
+});
+
+app.listen(3000, function() {
+    console.log('Node server listening on port 3000');
+});
