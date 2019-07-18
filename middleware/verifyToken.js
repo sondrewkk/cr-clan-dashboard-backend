e52fbd07357e810b25f65126baaeed07e1da95a9
Sondre W. K-Knutsen <knutsensondre@gmail.com>
2019-07-19 00:29:11 +0200

    Added authorization bearer as header

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,17 +1,19 @@
 const jwt = require('jsonwebtoken');
 
-modeule.exports = function(req, res, next) {
-  const token = req.header('auth-token');
+module.exports = function(req, res, next) {
+  let token = req.header('Authorization');
 
-  if (!token) {
-    return res.status(401).send('Access Denied');
-  }
+  if (token.startsWith('Bearer ')) {
+    token = token.slice(7, token.length);
 
-  try {
-    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = verified;
-    next();
-  } catch (err) {
-    res.status(400).send('Invalid token');
+    try {
+      const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+      req.user = verified;
+      next();
+    } catch (err) {
+      res.status(400).send('Invalid token');
+    }
+  } else {
+    return res.status(401).send('Bearer token is not supplied');
   }
 };
