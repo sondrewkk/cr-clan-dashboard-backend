c08bc53a82a8a4e346845de6d7d574292bcbc31f
Sondre <knutsensondre@gmail.com>
2020-01-02 14:30:19 -0800

    Fixed missing curlybrace

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -7,21 +7,22 @@ function authorize(roles = []) {
   if(typeof roles === 'string') {
     roles = [roles];
   }
-}
 
-return [
-  // authenticate JWT token and attach user to request object (req.user)
-  jwt({ secret: process.env.TOKEN_SECRET }),
 
-   // authorize based on user role
-  (req, res, next) => {
-    if (roles.length && !roles.includes(req.user.role)) {
-      return res.status(401).json({message: 'Unauthorized'});
+  return [
+    // authenticate JWT token and attach user to request object (req.user)
+    jwt({ secret: process.env.TOKEN_SECRET }),
+
+    // authorize based on user role
+    (req, res, next) => {
+      if (roles.length && !roles.includes(req.user.role)) {
+        return res.status(401).json({message: 'Unauthorized'});
+      }
+
+      // authentication and authorization successful
+      next();
     }
-
-    // authentication and authorization successful
-    next();
-  }
-];
+  ];
+}
 
 module.exports = authorize;
